22c715208d546fe7b33e732f9b42c94341210c62
reganperkins <reganlperkins@gmail.com>
2021-08-29 21:26:37 -0700

    add more details copy

--- src/components/App/app.tsx
+++ src/components/App/app.tsx
@@ -18,7 +18,7 @@ function App() {
             <p className={styles.slug}>Hello. This is</p>
             <h1>Regan Little</h1>
             <p>(n.) /Ray-gun/</p>
-            <p>defn. a <strong>Senior Frontend Engineer</strong> who is passionate about building beautiful, performant, and accessible applications. Always curious and in love with the challenge of learning new technologies —I enjoy writing clean, concise, functional code that provides the best user experience possible.</p>
+            <p>defn. a <strong>Senior Front-end Engineer</strong> who is passionate about building beautiful, performant, and accessible applications. Always curious and in love with the challenge of learning new technologies —I enjoy writing clean, concise, functional code that provides the best user experience possible.</p>
             <WaveButton />
           </div>
           <div className={`${styles.headerImage} half`}>
@@ -48,6 +48,73 @@ function App() {
           <h2 className="section-padding">Achievements</h2>
 
           <Project 
+            title="Modernizing"
+            description="Dribbble is a ten year old Rails app. In the time I have been here we have made massive gains in feature release cadence, increasing code clarity and onboarding new developers"
+            imageName="login.png"
+            containerClass="right-section"
+            topics={[
+              {
+                title: 'Storybook',
+                description: 'Building UI components and pages in isolation makes it easier to QA, add new behavior after a component has been made, and develop without interference.'
+              },
+              {
+                title: 'Tooling',
+                description: 'I set up our Eslint, Prettier, and increased usage of tools like Bundle Wizard and Lighthouse which have made our site more preferment and code consistent.'
+              },
+              {
+                title: 'Vue',
+                description: 'Vue has made our development easier with the addition of a component library that matches our design system, better testing tools, built in reactivity and easy readability.'
+              },
+              {
+                title: 'Developer guide',
+                description: 'I helped to create our internal development guide which helps onboard new users. It outlines important concepts like lazyloading, responsive images, using scoped styles and patterns for testing.'
+              },
+            ]}
+          />
+
+          <Project 
+            title="Dribbble 1.5"
+            description="A completely new look for Dribbble, this was a massive under takeing with a fairly tight deadline. It involved working closely with many parties including project managers, leadership and the design team to plan and execute."
+            imageName="dribbble-1-5.png"
+            containerClass="left-section"
+            topics={[
+              {
+                title: 'First major update in 10 years',
+                description: 'I worked to update all our color schemes, form elements, icons, as well as our default padding, margins, and basic elements. Because we needed to launch this all at once we used a webpack-level feature flag to keep showing the old styles well building the new.'
+              },
+              {
+                title: 'A shared ecosystem',
+                description: 'Previous to the re-skin Dribbble did not have a set design system. This meant that every time a dropdown was needed there was a good chance a new one would be created. In order to put an end to the madness and ensure that design and engineering where in sync I worked to create a shared design system and component library. This has since greatly improved our development time and cohesiveness of our site.',
+              }
+            ]}
+          />
+
+          <Project 
+            title="Marketplace ...coming soon"
+            description="Front-end lead on the biggest change to our "
+            imageName="dribbble-1-5.png"
+            containerClass="right-section"
+            topics={[
+              {
+                title: 'Goods upload flow',
+                description: 'A complete re-write of our upload flow, this change also splits a \'good\' into its own first world citizen.'
+              },
+              {
+                title: 'A central marketplace',
+                description: 'Created a new marketplace hub with suggested goods, daily features, filters and category pages.'
+              },
+              {
+                title: 'Admin queue',
+                description: 'Mentored a junior dev on using Vue, project scoping and helped them create our new admin review queue for posted goods.'
+              },
+              {
+                title: 'Refactoring old (hairy) code',
+                description: 'Our concept of shots and shots as a good is old highly coupled code. I took this opportunity to refactor and cleanup that logic.'
+              },
+            ]}
+          />
+
+          <Project 
             title="Update Navigation"
             description=""
             imageName="nav.png"
@@ -69,118 +136,60 @@ function App() {
           />
 
           <Project 
-            title="Modernizing"
-            description="Dribbble is a ten year old Rails app. In the time I have been here we have made massive gains in feature release cadence, increasing code clarity and onboarding new developers"
-            imageName="login.png"
-            containerClass="right-section"
-            topics={[
-              {
-                title: 'Storybook',
-                description: 'Building UI components and pages in isolation makes it easier to QA, add new behavior after a component has been made, and develop without interference.'
-              },
-              {
-                title: 'Tooling',
-                description: 'I set up our Eslint, Prettier, and increased usage of tools like Bundle Wizard and Lighthouse which have made our site more preferment and code consistent.'
-              },
-              {
-                title: 'Vue',
-                description: 'Vue has made our development easier with the addition of a component library that matches our design system, better testing tools, built in reactivity and easy readability.'
-              },
-              {
-                title: 'Developer guide',
-                description: 'Created our internal development guide which helps onboard new users. It outlines important concepts like lazyloading, responsive images, using scoped styles and patterns for testing.'
-              },
-            ]}
-          />
-
-          <Project 
-            title="Dribbble 1.5"
-            description="A completely new look for Dribbble, this was a massive under takeing with a fairly tight deadline. It involved working closely with many parties including project managers, leadership and the design team to plan and execute."
-            imageName="dribbble-1-5.png"
-            containerClass="left-section"
-            topics={[
-              {
-                title: 'First major update in 10 years',
-                description: 'We updated all our color schemes, form elements, icons, as well as our default padding, margins, and basic elements. Because we needed to launch this all at once we used a webpack-level feature flag to keep showing the old styles well building the new.'
-              },
-              {
-                title: 'A shared ecosystem',
-                description: 'Previous to the re-skin Dribbble did not have a set design system. This meant that every time a dropdown was needed there was a good chance a new one would be created. In order to put an end to the madness and ensure that design and engineering where in sync we worked to create a shared design system and component library. This has since greatly improved our development time and cohesiveness of our site.',
-              }
-            ]}
-          />
-
-          <Project 
             title="Designer Search"
-            description="One of Dribbbles bigger projects and one I was lead on. This turned out to be one of the biggest revenue streams for Dribbble; helping our users find the perfect fit for their company."
+            description="One of Dribbble's bigger projects and one I was lead on. This turned out to be one of the biggest revenue streams for Dribbble; helping our users find the perfect fit for their company."
             imageName="login.png"
             containerClass="right-section"
             topics={[
               {
-                title: '',
-                description: ''
+                title: 'Saved Searches',
+                description: 'Allow clients to name and save their filters, bookmarks and conversations for perspective roles.'
               },
               {
-                title: '',
-                description: ''
+                title: 'Advanced Filtering',
+                description: 'Allows for the filtering of talent based on their settings and preferences.'
               },
               {
-                title: '',
-                description: ''
-              },
-              {
-                title: '',
-                description: ''
+                title: 'Responsive',
+                description: 'This design works flawlessly on desktop and mobile platforms and utilizes responsive images and lazyloading.'
               },
             ]}
           />
 
           <Project 
             title="Job Board"
-            description=""
+            description="Though a simple project I scoped, planned and created these pages and filters that showcase jobs relevant to our community."
             imageName="job-board.jpeg"
             containerClass="left-section"
             topics={[
               {
-                title: '',
-                description: ''
+                title: 'Posting jobs',
+                description: 'Allow the posting of new roles when a user purchases a job board listing. Also creating job post details pages.'
               },
               {
-                title: '',
-                description: ''
-              },
-              {
-                title: '',
-                description: ''
-              },
-              {
-                title: '',
-                description: ''
+                title: 'Filtering roles',
+                description: 'Created param based filters to help users find the jobs they want.'
               },
             ]}
           />
 
           <Project 
             title="Workshops"
-            description=""
+            description="A new addition to our revenue stream, Dribbble workshops are added frequently and we needed a solution that enabled us to quickly add new events without taking developer time to implement"
             imageName="learn.png"
             containerClass="right-section"
             topics={[
               {
-                title: '',
-                description: ''
+                title: 'Quick rollout of new events',
+                description: 'I utilized ERB templates and a yaml file to easily create new pages.'
               },
               {
-                title: '',
-                description: ''
+                title: 'Responsive layout',
+                description: 'I created the templates for the index and workshop pages, these pages.'
               },
               {
-                title: '',
-                description: ''
-              },
-              {
-                title: '',
-                description: ''
+                title: 'Eventbrite',
+                description: 'Set up Eventbrite as an intermediary way of collecting payments well we build our own.'
               },
             ]}
           />
@@ -192,32 +201,20 @@ function App() {
             containerClass="left-section"
             topics={[
               {
-                title: '',
-                description: ''
+                title: 'Google Authentication',
+                description: 'I implemented Google One tap for quick and easy logins, this increased our login rate dramatically'
               },
               {
-                title: '',
-                description: ''
-              },
-              {
-                title: '',
-                description: ''
-              },
-              {
-                title: '',
-                description: ''
+                title: 'Clean User Interface',
+                description: 'With logins simplicity is key, the updated login and sign up pages are clear, clean and convenient'
               },
             ]}
           />
-
-
-          {/* <Project 
-            title="performance optimizations"
-          /> */}
-          {/* <Project 
-            title="marketplace ...coming soon"
-          /> */}
         </section>
+
+        {/* <Project 
+          title="marketplace ...coming soon"
+        /> */}
 
         <section id="contact" className="flex">
           <div className={`${styles.contactArtboard} half`}>
@@ -227,11 +224,11 @@ function App() {
           <div className="half">
             <h2>Lets talk</h2>
             <p className={styles.subTitle}>I'd love to hear from you</p>
-            {/* <p>
+            <p>
               <a className="block" href="mailto:reganllittle@gmail.com">reganllittle@gmail.com</a>
               <a className="block" href="tel:+1-604-992-7526">(604) 992-7526</a>
-              <a href="https://github.com/reganperkins">View more</a>
-            </p> */}
+              <a href="https://github.com/reganperkins">Github</a>
+            </p>
             <form className={styles.contactForm}>
               <input type="text" placeholder="Name"></input>
               <input type="email" placeholder="Email"></input>
