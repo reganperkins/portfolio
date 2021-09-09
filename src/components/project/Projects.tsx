706590dbfc59d26ce4c718f663412ee9a0864ffd
reganperkins <reganlperkins@gmail.com>
2021-09-08 22:38:30 -0700

    pass common styles

--- src/components/project/Projects.tsx
+++ src/components/project/Projects.tsx
@@ -3,9 +3,13 @@ import Project  from './Project';
 import styles from "./Projects.module.scss";
 import { ReactComponent as BlobSlant } from './assets/blob-slant.svg';
 
-function App() {
+interface ProjectsProp {
+  sectionClass: string;
+}
+
+function App(props: ProjectsProp) {
   return (
-    <section id="work" className={styles.sectionMargin}>
+    <section id="work" className={props.sectionClass}>
       <h2 className="section-padding">Achievements</h2>
 
       <Project 
