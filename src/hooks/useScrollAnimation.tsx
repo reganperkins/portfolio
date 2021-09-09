fe202145e044799070ac42f5643c95ed907dd5d7
reganperkins <reganlperkins@gmail.com>
2021-09-09 10:29:24 -0700

    testing idea for scroll hook

--- src/hooks/useScrollAnimation.tsx
+++ src/hooks/useScrollAnimation.tsx
@@ -1,8 +1,32 @@
-import React, { useLayoutEffect, useRef, useState } from "react";
+import { useLayoutEffect, useState } from "react";
 
-function useScrollAnimation(element: JSX.Element | JSX.Element[]) {
-  console.log("This is the element received: " + element);
+function useScrollAnimation(refEl: any) {
+  console.log("This is the target received: " + refEl);
 
+  const [show, setShow] = useState(false);
+  const [percentShown, setPercentShow] = useState(0);
+  const target = refEl.current;
+
+  useLayoutEffect(() => {
+    const targetPos = target.getBoundingClientRect().top;
+    const targetHeight = target.offsetHeight;
+
+    const onScroll = () => {
+      const scrollPos = window.scrollY + window.innerHeight;
+      if (targetPos < scrollPos) {
+        setShow(() => true);
+        let itemPercent = ((scrollPos - targetPos) * 100) / targetHeight;
+        if (itemPercent > 100) itemPercent = 100;
+        if (itemPercent < 0) itemPercent = 0;
+        setPercentShow(() => itemPercent);
+      } else if (targetPos > scrollPos) {
+        setShow(() => false);
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [target]);
 }
 
 export default useScrollAnimation;
