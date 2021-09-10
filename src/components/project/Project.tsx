1387b527607e19c361bd2e84fe8d36489b497c80
reganperkins <reganlperkins@gmail.com>
2021-09-09 22:34:54 -0700

    split projects into own component

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -1,22 +1,35 @@
-import styles from "./Project.module.scss";
 import GridItem from './GridItem'
+import { ReactComponent as BlobSlant } from './assets/blob-slant.svg';
+import styles from "./Projects.module.scss";
 
 interface ProjectProp {
   title: string;
   description?: string;
+  color: string;
+  desktopImage: string;
   containerClass?: string;
   topics: { title: string; description: string; }[];
   children?: JSX.Element | JSX.Element[];
 }
 
 function Project(props: ProjectProp) {
+  let blob;
+  let blobClass 
+  if (props.containerClass === 'right-section') {
+    blobClass = styles.blobRight;
+  } else if (props.containerClass === 'left-section') {
+    blobClass = styles.blobLeft;
+  }
+
+  if (blobClass) {
+    blob = <BlobSlant className={`${styles.blob} ${styles[props.color]} ${blobClass}`}/>
+  }
 
   return (
     <article className={`${props.containerClass} project-container section-padding`}>
       <div className="project-content">
         <h3>{ props.title }</h3>
         <p>{ props.description }</p>
-
         <div className="project-grid">
           {props.topics.map((topic) =>
             <GridItem
@@ -27,9 +40,17 @@ function Project(props: ProjectProp) {
           )}
         </div>
       </div>
-      {props.children}
+      <div className="project-display">
+        { blob }
+        <img src={`/images/projects/${props.desktopImage}`} className={styles.projectDesktopImage} alt={props.title} />
+        { props.children }
+      </div>
     </article>
   );
 }
+
+Project.defaultProps = {
+  color: 'blue',
+} as Partial<ProjectProp>;
 
 export default Project;
--- src/hooks/useScrollAnimation.tsx
+++ src/hooks/useScrollAnimation.tsx
@@ -4,7 +4,7 @@ function useScrollAnimation(refEl: any) {
   console.log("This is the target received: " + refEl);
 
   const [show, setShow] = useState(false);
-  const [percentShown, setPercentShow] = useState(0);
+  const [showPercent, setShowPercent] = useState(0);
   const target = refEl.current;
 
   useLayoutEffect(() => {
@@ -18,7 +18,7 @@ function useScrollAnimation(refEl: any) {
         let itemPercent = ((scrollPos - targetPos) * 100) / targetHeight;
         if (itemPercent > 100) itemPercent = 100;
         if (itemPercent < 0) itemPercent = 0;
-        setPercentShow(() => itemPercent);
+        setShowPercent(() => itemPercent);
       } else if (targetPos > scrollPos) {
         setShow(() => false);
       }
@@ -27,6 +27,11 @@ function useScrollAnimation(refEl: any) {
     window.addEventListener("scroll", onScroll);
     return () => window.removeEventListener("scroll", onScroll);
   }, [target]);
+
+  return {
+    show,
+    showPercent,
+  }
 }
 
 export default useScrollAnimation;
